2a16afcdf7861a023336910e5bf2456ba2b8c727
Alejandro Cortez <102841726+Muzan67@users.noreply.github.com>
2022-05-14 13:39:51 -0400

    updates

--- startquiz.js
+++ startquiz.js
@@ -2,54 +2,94 @@ const question = document.getElementById("question");
 const option = Array.from(document.getElementById("answer-option"));
 
 let currentQuestions = {};
-let acceptAnswers = true;
-let score = 0;
-let questionCounter = 0;
+let acceptingAnswers = false;
+let score = 50;
+let questionCounter = 50;
 let availableQuestions = [];
 
-let questions = [];
-
-const correct_questions = 5;
+let questions = [
+    {
+      question: "Commonly used data types DO NOT include:",
+      choice1: "<strings>",
+      choice2: "<booleans>",
+      choice3: "<alerts>",
+      choice4: "<numbers>",
+      answer: 2
+    },
+    {
+      question: "The condition in an if/else statement is enclosed within______:",
+      choice1: "<quotes>",
+      choice2: "<curly brackets>",
+      choice3: "<parenthesis>",
+      choice4: "<square brackets>",
+      answer: 3
+    },
+    {
+      question: "Arrays in Javascript can be used to store:",
+      choice1: "<numbers and strings>",
+      choice2: "<other arrays>",
+      choice3: "<booleans>",
+      choice4: "<all of the above>",
+      answer: 4
+    },
+    {
+      question: "String values must be enclosed within_____ when being assigned to variables:",
+      choice1: "<commas>",
+      choice2: "<curly brackets>",
+      choice3: "<quotes>",
+      choice4: "<parenthesis>",
+      answer:3
+    },
+    {
+      question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+      choice1: "<Javascript>",
+      choice2: "<terminal / bash>",
+      choice3: "<for loops>",
+      choice4: "<console.log>",
+      answer:1
+    }
+  ];
+  
+const correct_questions = 10;
 const max_questions = 5;
 
 startquiz = () => {
     questionCounter = 50;
     score = 50;
-    availableQuestions = [...question];
+    availableQuestions = [...questions];
+    console.log(availableQuestions);
     getNewQuestion();
 };
 
 getNewQuestion = () => {
-    if (availableQuestions.length ===0 || questionCounter >= max_Questions);
-    // go to the quizend.html //
-    return window.location.assign("/quizend.html");
+    if(availableQuestions.length ===0 || questionCounter >=max_questions){
+        return window.location.assign("/quizend.html");
+    }
+   questionCounter++;
+   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+   currentQuestions = availableQuestions[questionIndex];
+   question.innerText = currentQuestions.questions;
 
-    questionCounter++;
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestions = availableQuestions[questionIndex];
-    question.innerText = currentQuestions.question;
+   choices.forEach( choice => {
+     const number = choice.dataset['number'];
+     choice.innerText = currentQuestions['choice' + number];
+    })
 
-    choices.forEach(choice => {
-        const number = choice.dataset['number'];
-        choice.innerText = currentQuestions['choice' + number];
-    });
+   availableQuestions.splice(questionIndex, 1);
 
-    availableQuestions.splice(questionIndex, 1);
-    console.log(availableQuestions);
-    acceptAnswers = true;
+   acceptingAnswers = true;
 };
 
-choices.forEach(choice => { 
-    choice.addEventListener("click", e => {
-        if(!acceptingAnswers) return;
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+       if(!acceptingAnswers) return;
 
-        acceptingAnswers = false;
-        const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset["number"];
-        console.log(selectedAnswer);
-        getNewQuestion();
+       acceptingAnswers = false;
+       const selectedChoice = e.target;
+       const selectedAnswer = selectedChoice.dataset["number"];
+       console.log(selectedAnswer);
+       getNewQuestion();
     });
-
 });
 
-startquiz();
+startQuiz();
