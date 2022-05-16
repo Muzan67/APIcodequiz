f4574ec23a369dbe981d2f21efed03f878670443
Alejandro Cortez <102841726+Muzan67@users.noreply.github.com>
2022-05-16 03:43:46 -0400

    updating HW

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # APIcodequiz
+We were tasked to create an interactive time coding assessment with a combination of multiple-choice questions with answers
+The app will run in our browser and feature HTML, CSS , and JavaScript code. 
+
+# Installation
+I tried my best to complete my work but I had many trouble getting the questions to launch. 
+
+I was able to style and implement some functions of what was expected for this assignment. 
+
+# Credit
+
+https://www.w3schools.com/ - I used the website for reference which helped me choose the correct array for my functions.
+
+https://developer.chrome.com/docs/devtools/open/ - I used the Console tab in Google Chrome Developer Tools to troubleshoot and correct my code as I was on working on.
+
+https://stackoverflow.com/questions - I used StackOverflow to see the approach and opinions of the community of people and see how I can apply their knowledge to my work.
+
+https://html-color.codes/purple - I used HTML-Color.Codes for giving color to my page.
+
+https://fonts.google.com/specimen/Poppins query=poppins#standard-styles - I used to style the writing in my style.css
--- script.js
+++ script.js
@@ -1,72 +1,145 @@
-// const portions
+// countdown timer //
+const startingSeconds = 50;
+let time = startingSeconds * 50;
 
-const _question = document.getElementById('question');
-const _options = document.getElementById('.quiz-options');
-const _correctScore = document.getElementById('correct-score');
-const _totalQuestion = document.getElementById('total-question');
-const _checkButton = document.getElementById('check-answer');
-const _playAgainButton = document.getElementById('play-again');
-const _result = document.getElementById('result');
+const countdownEl= document.getElementById('countdown');
 
-let correctAnswer = "", correctScore = askedCount = 0, _totalQuestion = 5;
+setInterval(updateCountDown, 1000);
 
-// event listener // 
-function eventListeners(){
-  _checkButton.addEventListener('click', checkAnswer);
+function updateCountDown(){
+  let seconds = time % 50;
+
+  countdownEl.innerHTML = `${seconds}`;
+  time--;
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  loadQuestion();
-  eventListeners();
-  _totalQuestion.textContent = _totalQuestion;
-  _correctScore.textContent = correctScore;
+// const //
+
+const startQuizBtn = document.getElementById('startquiz-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+startQuizBtn.addEventListener('click', startQuiz)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
 })
 
-// display question and options //
-
-function showQuestion(data){
-  correctAnswer = data.correct_answer;
-  let incorrectAnswer = data.incorrect_answers;
-  let optionsList = incorrectAnswer;
-  optionsList.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1)), 0, correctAnswer);
-
-question.innerHTML = `${data.question} <br> <span class = "category'>${data.category} </span>`;
-_options.innerHTML = `
-    ${optionsList.map((option, index) => 
-      <li> ${index + 1}. <span> ${option} </span></li>
-      ).join('')}`;
-
-      selectedOption();
-
+function startQuiz() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 5
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
 }
 
-// options selection // 
-function selectedOption(){
-  _options.querySelectorAll('li').forEach((option) => {
-    option.addEventListener('click', () => {
-      if(_options.querySelector('.selected')){
-        const activeOption = _options.querySelector('selected');
-        activeOption.classList.remove('selected');
-      }
-      option.classList.add('selected');
-    });
-  });
-
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-function eventListeners(){
-  _checkButton.addEventListener('click', checkAnswer);
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
 }
 
-// check answers // 
-function checkAnswer(){
-  _checkButton.disabled = true;
-  if(_options.querySelector('.selected')){
-      let selectedAnswer = _options.querySelector('.selected span').textContent;
-      if(selectedAnswer == correctAnswer){
-        correctScore++;
-        _result.innerHTML = `<p> <i class = "fas fa-check"></i>Correct Answer! </P>`;
-      };
-  };
-
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
 }
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+// API Test Questions //
+
+const questions = [
+  {
+    question: 'Commonly used data types DO NOT include?',
+    answer: [
+    { choice1: 'strings', correct: false },
+    { choice2: 'booleans', correct: true },
+    { choice3: 'alerts', correct: false },
+    { choice4: 'numbers', correct: false },
+    ]
+  },
+  {
+    question: 'The condition in an if/else statement is enclosed within______?',
+    answer: [
+    { choice1: 'quotes', correct: false },
+    { choice2: 'curly brackets', correct: false },
+    { choice3: 'parenthesis', correct: true },
+    { choice4: 'square brackets', correct: false },
+    ]
+  },
+  {
+    question: "<Arrays in Javascript can be used to store?",
+    answer: [
+    { choice1: 'numbers and strings', correct: false },
+    { choice2: 'other arrays', correct: false },
+    { choice3: 'booleans', correct: false },
+    { choice4: 'all of the above', correct: true },
+    ]
+  },
+  {
+    question: "String values must be enclosed within_____ when being assigned to variables?",
+    answer: [
+    { choice1: 'commas', correct: false },
+    { choice2: 'curly brackets', correct: false },
+    { choice3: 'quotes', correct: true },
+    { choice4: 'parenthesis', correct: false },
+    ]
+  },
+  {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is?",
+    answer: [
+    { choice1: 'Javascript', correct: true },
+    { choice2: 'terminal / bash', correct: false },
+    { choice3: 'for loops', correct: false },
+    { choice4: 'console.log', correct: false },
+    ]
+  }
+]
+ 
