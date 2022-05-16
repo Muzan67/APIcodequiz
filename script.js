87e32854e590933d6145e38b52a1b94dc21351b0
Alejandro Cortez <102841726+Muzan67@users.noreply.github.com>
2022-05-15 23:57:51 -0400

    updating javascript

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const _result = document.getElementById('result');
 
 let correctAnswer = "", correctScore = askedCount = 0, _totalQuestion = 5;
 
+// event listener // 
 function eventListeners(){
   _checkButton.addEventListener('click', checkAnswer);
 }
@@ -20,3 +21,52 @@ document.addEventListener('DOMContentLoaded', () => {
   _totalQuestion.textContent = _totalQuestion;
   _correctScore.textContent = correctScore;
 })
+
+// display question and options //
+
+function showQuestion(data){
+  correctAnswer = data.correct_answer;
+  let incorrectAnswer = data.incorrect_answers;
+  let optionsList = incorrectAnswer;
+  optionsList.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1)), 0, correctAnswer);
+
+question.innerHTML = `${data.question} <br> <span class = "category'>${data.category} </span>`;
+_options.innerHTML = `
+    ${optionsList.map((option, index) => 
+      <li> ${index + 1}. <span> ${option} </span></li>
+      ).join('')}`;
+
+      selectedOption();
+
+}
+
+// options selection // 
+function selectedOption(){
+  _options.querySelectorAll('li').forEach((option) => {
+    option.addEventListener('click', () => {
+      if(_options.querySelector('.selected')){
+        const activeOption = _options.querySelector('selected');
+        activeOption.classList.remove('selected');
+      }
+      option.classList.add('selected');
+    });
+  });
+
+}
+
+function eventListeners(){
+  _checkButton.addEventListener('click', checkAnswer);
+}
+
+// check answers // 
+function checkAnswer(){
+  _checkButton.disabled = true;
+  if(_options.querySelector('.selected')){
+      let selectedAnswer = _options.querySelector('.selected span').textContent;
+      if(selectedAnswer == correctAnswer){
+        correctScore++;
+        _result.innerHTML = `<p> <i class = "fas fa-check"></i>Correct Answer! </P>`;
+      };
+  };
+
+}
